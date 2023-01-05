ce1c58a5a87fe52d59145ea90c2080d7bcf54f86
andreymakeenko <mrakas1993@gmail.com>
2023-01-05 20:25:02 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -31,10 +31,48 @@ window.addEventListener('DOMContentLoaded',()=>{
             })
         }
     })
+    //слайдер
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total');
+    let slideIndex = 1;
 
+    function showSlides(n){
+        if(n>slides.length){ 
+            slideIndex=1;
+        }
+        if(n<1){
+            slideIndex = slides.length;
+        }
 
+        if(slides.length<10){
+            total.textContent=`0${slides.length}`;
+        }else{
+            total.textContent = slides.length;
+        }
 
+        if(slideIndex < 10){
+            current.textContent = `0${slideIndex}`;
+        }else{
+            current.textContent = slideIndex;
+        }
+        slides.forEach(item => item.style.display ="none");
+        slides[slideIndex-1].style.display = "block";
+    }
+    showSlides(slideIndex);
 
+    function plusSlide(n){
+        showSlides(slideIndex+=n);//slideIndex = slideIndex + 1//2//3
+    }
+
+    next.addEventListener('click',()=>{
+        plusSlide(1);
+    })
+    prev.addEventListener('click',()=>{
+        plusSlide(-1);
+    })
 
 
 
